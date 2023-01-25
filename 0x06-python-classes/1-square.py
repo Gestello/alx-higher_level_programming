64f6c036e5332a3d23ab318a553a981cb53917e3
gestello <stellargodwin@gmail.com>
2023-01-25 02:42:01 +0000

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,7 +8,7 @@ class Square:
         __size (int): size of a side of the square
     	"""
 
-	def __init__(self, size):
+def __init__(self, size):
         """Initializes a square
         Args:
             size (int): size of a side of the square
