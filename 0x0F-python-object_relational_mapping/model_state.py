3e6f613d79ac2669380834bbc740267b356f98e4
gestello <stellargodwin@gmail.com>
2023-04-23 22:06:55 +0000

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" New class Geometry module """
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
 
+from sqlalchemy import (create_engine)
 
-class BaseGeometry:
-    """ Base Geometry """
-
-    def area(self):
-        raise Exception("area() is not implemented")
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
